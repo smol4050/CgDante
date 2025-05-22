476ee72c5c221cb30297116f6f845a18cb4732fb
Juanca25-10 <juan_c.lopez_s@uao.edu.co>
2025-05-22 18:52:08 -0500

    hyhy
    hyhy

--- Dante/Assets/AssetsMilo_CG/Scripts_Milo_CG/ScriptsGame_Milo/GameManager.cs
+++ Dante/Assets/AssetsMilo_CG/Scripts_Milo_CG/ScriptsGame_Milo/GameManager.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
     public static GameManager Instance;
 
-    public int objetosRecolectados = 0;
-    public int totalObjetos = 10;
+    public List<ProgresoNivel> progresoPorNivel = new List<ProgresoNivel>();
+    public int totalObjetosPorNivel = 10; // Puedes cambiar esto por nivel si deseas
 
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject); // Persiste entre escenas
+            DontDestroyOnLoad(gameObject);
+            InicializarProgreso();
         }
         else
         {
@@ -22,22 +25,98 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void InicializarProgreso()
+    {
+        // Agrega aquí los nombres REALES de tus escenas
+        progresoPorNivel.Add(new ProgresoNivel("Waen_CG"));
+        progresoPorNivel.Add(new ProgresoNivel("Frame_CG"));
+        progresoPorNivel.Add(new ProgresoNivel("Milo_CG"));
+        progresoPorNivel.Add(new ProgresoNivel("Smol_CG"));
+    }
+
+    public ProgresoNivel GetProgresoNivelActual()
+    {
+        string nivelActual = SceneManager.GetActiveScene().name;
+        return progresoPorNivel.Find(p => p.nombreNivel == nivelActual);
+    }
+
     public void SumarObjeto()
     {
-        objetosRecolectados++;
+        ProgresoNivel progreso = GetProgresoNivelActual();
+        if (progreso == null) return;
 
-        Debug.Log($"Objetos recolectados: {objetosRecolectados}/{totalObjetos}");
+        progreso.objetosRecolectados++;
+        Debug.Log($"[{progreso.nombreNivel}] Objetos recolectados: {progreso.objetosRecolectados}/{totalObjetosPorNivel}");
 
-        if (objetosRecolectados >= totalObjetos)
+        if (progreso.objetosRecolectados >= totalObjetosPorNivel)
         {
             Debug.Log("¡Todos los objetos recolectados!");
-            // Activar puerta final o evento
+            // Aquí puedes llamar algo como: GameController.Instance.PuertaFinalDisponible();
         }
     }
 
+    public int ObtenerObjetosRecolectados()
+    {
+        ProgresoNivel progreso = GetProgresoNivelActual();
+        return progreso != null ? progreso.objetosRecolectados : 0;
+    }
+
+    public void CompletarNivel()
+    {
+        ProgresoNivel progreso = GetProgresoNivelActual();
+        if (progreso != null)
+        {
+            progreso.nivelCompletado = true;
+            Debug.Log($"Nivel completado: {progreso.nombreNivel}");
+        }
+    }
+
+    public void ReiniciarObjetosDelNivelActual()
+    {
+        ProgresoNivel progreso = GetProgresoNivelActual();
+        if (progreso != null)
+            progreso.objetosRecolectados = 0;
+    }
 
     public void CambiarEscena(string nombreEscena)
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(nombreEscena);
+        SceneManager.LoadScene(nombreEscena);
     }
 }
+    //public static GameManager Instance;
+
+    //public int objetosRecolectados = 0;
+    //public int totalObjetos = 40;
+
+    //private void Awake()
+    //{
+    //    if (Instance == null)
+    //    {
+    //        Instance = this;
+    //        DontDestroyOnLoad(gameObject); // Persiste entre escenas
+    //    }
+    //    else
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
+
+    //public void SumarObjeto()
+    //{
+    //    objetosRecolectados++;
+
+    //    Debug.Log($"Objetos recolectados: {objetosRecolectados}/{totalObjetos}");
+
+    //    if (objetosRecolectados >= totalObjetos)
+    //    {
+    //        Debug.Log("¡Todos los objetos recolectados!");
+    //        // Activar puerta final o evento
+    //    }
+    //}
+
+
+    //public void CambiarEscena(string nombreEscena)
+    //{
+    //    UnityEngine.SceneManagement.SceneManager.LoadScene(nombreEscena);
+    //}
+//}
--- Dante/Assets/AssetsMilo_CG/Scripts_Milo_CG/ScriptsGame_Milo/PuertaCambioEscena.cs
+++ Dante/Assets/AssetsMilo_CG/Scripts_Milo_CG/ScriptsGame_Milo/PuertaCambioEscena.cs
@@ -6,8 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class PuertaCambioEscena : MonoBehaviour, IInteractuable
 {
+    public string nombreEscena; // Nombre de la escena a la que quieres cambiar
     public void ActivarObjeto()
     {
-        SceneManager.LoadScene("Smol_CG"); // Asegúrate que el nombre esté bien escrito
+        SceneManager.LoadScene(nombreEscena); // Asegúrate que el nombre esté bien escrito
     }
 }
