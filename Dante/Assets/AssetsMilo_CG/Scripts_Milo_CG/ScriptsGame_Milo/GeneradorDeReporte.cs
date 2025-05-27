f9601897a0aab710131a709a5f21be2f7f81d3c4
Juanca25-10 <juan_c.lopez_s@uao.edu.co>
2025-05-27 17:33:33 -0500

    CambiosFinales
    dcdce

--- Dante/Assets/AssetsMilo_CG/Scripts_Milo_CG/ScriptsGame_Milo/GeneradorDeReporte.cs
+++ Dante/Assets/AssetsMilo_CG/Scripts_Milo_CG/ScriptsGame_Milo/GeneradorDeReporte.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
-using UnityEngine.tvOS;
 
 /// <summary>
 /// Clase que genera un reporte del progreso de niveles.
